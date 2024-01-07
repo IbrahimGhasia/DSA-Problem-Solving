c983967138a37bf08478f4a24bf7ee7d713b79d5
IbrahimGhasia <ibrahim.ghasia@gmail.com>
2024-01-07 09:54:38 +0530

    LeetCode - 682

--- Leetcode/Arrays/BaseballGame.java
+++ Leetcode/Arrays/BaseballGame.java
@@ -1,5 +1,35 @@
+import java.util.Stack;
+
 public class BaseballGame {
+
+    public static int calPoints(String[] operations) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String s : operations) {
+            if (s.equals("+")) {
+                stack.push(stack.peek() + stack.elementAt(stack.size() - 2));
+            } else if (s.equals("D")) {
+                stack.push(stack.peek() * 2);
+            } else if (s.equals("C")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+
+        int result = 0;
+
+        for (int i : stack) {
+            result += i;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String[] op = { "5", "2", "C", "D", "+" };
+        int res = calPoints(op);
+        System.out.println(res);
     }
 }
